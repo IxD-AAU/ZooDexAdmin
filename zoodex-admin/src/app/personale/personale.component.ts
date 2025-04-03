33af02f3103eafbec868f692a9e054a864217e17
Casper Gramkov Boejen <cbojen22@student.aau.dk>
2025-04-03 11:40:41 +0200

    Created Login Page, Personale Page and created the login system without database.

---
+++ zoodex-admin/src/app/personale/personale.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personale',
+  imports: [],
+  templateUrl: './personale.component.html',
+  styleUrl: './personale.component.css'
+})
+export class PersonaleComponent {
+
+}

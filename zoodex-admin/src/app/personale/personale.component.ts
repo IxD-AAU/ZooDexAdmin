a4d9cfb503f5befbb5bce621e157ea172643eff3
Casper Gramkov Boejen <cbojen22@student.aau.dk>
2025-04-07 10:03:30 +0200

    Fixed Personale

--- zoodex-admin/src/app/personale/personale.component.ts
+++ zoodex-admin/src/app/personale/personale.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { SidebarService } from '../sidebar.service';
 import { PersonaleGetterService } from '../personale-getter.service';
@@ -6,7 +7,7 @@ import { PersonaleGetterService } from '../personale-getter.service';
 
 @Component({
   selector: 'app-personale',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CommonModule],
   templateUrl: './personale.component.html',
   styleUrl: './personale.component.css'
 })
@@ -19,8 +20,12 @@ export class PersonaleComponent implements OnInit {
   constructor(public sidebarService: SidebarService, public readonly personaleGetterService: PersonaleGetterService){ }
 
   ngOnInit(): void {
-      this.sidebarService.open();
+    setTimeout(() => {
       this.pageText = this.personaleGetterService.personale;
+      this.isDataReady = true;
       console.log(this.pageText);
+    }, 1000);
+
+
   }
 }

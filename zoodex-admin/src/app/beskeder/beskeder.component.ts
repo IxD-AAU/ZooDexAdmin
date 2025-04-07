21af50b0cce570fe6754f224bd48e50d6cb961ce
David031999 <gaming.daviddang@gmail.com>
2025-04-07 10:18:37 +0200

    unfucked

--- zoodex-admin/src/app/beskeder/beskeder.component.ts
+++ zoodex-admin/src/app/beskeder/beskeder.component.ts
@@ -1,8 +1,11 @@
+import { Component,OnInit } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { SidebarService } from '../sidebar.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-beskeder',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './beskeder.component.html',
   styleUrl: './beskeder.component.css'
 })
